5390cc2cca4cf591a9570653bb1eaa6f5a928147
HiroUrata <1712026@i.softbank.jp>
2021-08-08 14:28:23 +0900

    SlideViewの作成　(未スライド)

--- slideViewLesson/ViewController.swift
+++ slideViewLesson/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var slideView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        createSlideView()
+        
+        
+        
     }
 
+    func createSlideView(){
+        
+        slideView = UIView(frame: CGRect(x: self.view.frame.minX - self.view.frame.midX, y: self.view.frame.minY + 80, width: self.view.frame.size.width / 2, height: self.view.frame.size.height - 80))
+    
+        
+        slideView.backgroundColor = UIColor.systemGray
 
+        self.view.addSubview(slideView)
+    }
+
+    
+    
+    
+    @IBAction func slide(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 }
 
