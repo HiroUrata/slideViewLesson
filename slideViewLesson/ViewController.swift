972ac8e02827e2a06e8ba9e5dc09df7ba9f555c9
HiroUrata <1712026@i.softbank.jp>
2021-08-08 15:48:16 +0900

    slideViewにButtonを追加

--- slideViewLesson/ViewController.swift
+++ slideViewLesson/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     var slideView = UIView()
     
     override func viewDidLoad() {
@@ -29,7 +30,7 @@ class ViewController: UIViewController {
 
     func createSlideView(){
         
-        slideView = UIView(frame: CGRect(x: self.view.frame.minX - self.view.frame.midX, y: self.view.frame.minY + 80, width: self.view.frame.size.width / 2, height: self.view.frame.size.height - 80))
+        slideView = UIView(frame: CGRect(x: self.view.frame.minX - self.view.frame.midX, y: self.view.frame.minY + 80, width: self.view.frame.size.width / 2, height: self.view.frame.size.height - 160))
     
         
         slideView.backgroundColor = UIColor.systemGray
@@ -42,6 +43,7 @@ class ViewController: UIViewController {
         if slideView.frame.origin.x == self.view.frame.minX - self.view.frame.midX{
             
             UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {self.slideView.frame.origin.x = 0}, completion: nil)
+            createButton(xPoint: self.slideView.frame.origin.x)
             
         }else if slideView.frame.origin.x == 0{
             
@@ -51,7 +53,16 @@ class ViewController: UIViewController {
         
     }
     
-    
+    func createButton(xPoint:CGFloat){
+        
+        let button = UIButton(frame: CGRect(x: xPoint, y: slideView.frame.minY - 70, width: slideView.frame.size.width - 5, height: slideView.frame.size.height / 12))
+        
+        button.backgroundColor = UIColor.white
+        
+        slideView.addSubview(button)
+        
+    }
+   
     
     @IBAction func slide(_ sender: UIButton) {
         
